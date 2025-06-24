42bf53f62fc0586b358c00301a1d8f9b0417c777
maurice <mauricedaller@gmail.com>
2025-06-24 11:30:09 +0100

    pool qc drop and justification

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ train =pd.read_csv('data/train.csv')
 #print(train.head())
 
 #print(train.columns.tolist())
-print("pool qc data:")
-print(train['PoolQC'].isnull().value_counts())
-print(train.shape)
+
+#print(f"{column} data:")
+print(train.isna().sum().sort_values(ascending=False))
+print("pool unique")
+print(train['PoolArea'].unique())
+print((train['PoolArea'] == 0).sum())
+#pool area has 0 for no pool and matches the PoolQC NaN values
+#so we can drop PoolQC
+train.drop(['PoolQC'])
